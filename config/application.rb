9b81a7c2b91e48e6c3828025944694fa1b756ec6
Tyler Marien <tyler.marien@gmail.com>
2014-05-16 08:14:31 -0600

    Removed controller specific coffeescript files, added config to stop generating controller specific asset files

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module WorldCupDraft
   class Application < Rails::Application
+    
+    config.generators do |g|
+      g.assets false
+    end
+        
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

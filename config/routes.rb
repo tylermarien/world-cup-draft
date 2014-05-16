d1842c30606ea8ace1f6487ab1df8efc9b46c78e
Tyler Marien <tyler.marien@gmail.com>
2014-05-15 22:00:57 -0600

    Created Country and Player scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :players
+
+  resources :countries
+
   root 'welcome#index'
   
   # The priority is based upon order of creation: first created -> highest priority.

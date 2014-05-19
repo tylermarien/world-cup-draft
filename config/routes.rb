5c4c8b7655ecd98e853d2810bca5e3c37a17a9c1
Tyler Marien <tyler.marien@gmail.com>
2014-05-19 12:58:38 -0600

    Created Team scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :teams
+
   devise_for :users
   resources :matches
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519174922) do
+ActiveRecord::Schema.define(version: 20140519185600) do
 
   create_table "countries", force: true do |t|
     t.string   "name"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20140519174922) do
   end
 
   add_index "players", ["country_id"], name: "index_players_on_country_id"
+
+  create_table "teams", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

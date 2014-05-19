9f0987b09eb48474ebcfa805ab4c6f425b65f9dd
Tyler Marien <tyler.marien@gmail.com>
2014-05-19 12:41:32 -0600

    Added authenticate filter to controllers

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,4 +1,5 @@
 class CountriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_country, only: [:show, :edit, :update, :destroy]
 
   # GET /countries
--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,5 @@
 class MatchesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_match, only: [:show, :edit, :update, :destroy]
 
   # GET /matches

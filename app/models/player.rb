4bf748628b16953b646e06a308091208a4bce3b4
Tyler Marien <tyler.marien@gmail.com>
2014-05-19 20:20:30 -0600

    Added number_of_goals to Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,11 @@
 class Player < ActiveRecord::Base
   belongs_to :country
+  has_many :goals
   has_and_belongs_to_many :teams
+  
+  def number_of_goals
+    goals.count
+  end
   
   def to_s
     name
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -4,4 +4,9 @@ class PlayerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "get number of goals" do
+    messi = players(:messi)
+    assert_equal 1, messi.number_of_goals
+  end
 end

9d970b19c9d408e48277cb59ccccbebaea78b172
Tyler Marien <tyler.marien@gmail.com>
2014-06-09 22:27:21 -0600

    Added home_goals to Match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,11 @@
 class Match < ActiveRecord::Base
   belongs_to :home, class_name: "Country"
   belongs_to :away, class_name: "Country"
+  has_many :goals
+  
+  def home_goals
+    self.goals.where(country_id: self.home.id).count
+  end
   
   def to_s
     home.name + " vs " + away.name
--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class MatchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should return the correct number of goals scored by home team" do
+    assert_equal 1, matches(:one).home_goals
+  end
+
 end

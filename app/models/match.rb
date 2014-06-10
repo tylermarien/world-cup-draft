02b1eb7a43d8ccdaa94c54566f6319b429d820cc
Tyler Marien <tyler.marien@gmail.com>
2014-06-09 22:43:08 -0600

    Added winning_country to Match

--- app/models/match.rb
+++ app/models/match.rb
@@ -9,7 +9,13 @@ class Match < ActiveRecord::Base
   
   def away_goals
     self.goals.where(country_id: self.away.id).count
-  end  
+  end
+  
+  def winning_country
+    return self.home if self.home_goals > self.away_goals
+    return self.away if self.away_goals > self.home_goals
+    return nil
+  end
   
   def to_s
     home.name + " vs " + away.name
--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -9,5 +9,9 @@ class MatchTest < ActiveSupport::TestCase
   test "should return the correct number of goals scored by away team" do
     assert_equal 0, matches(:one).away_goals
   end
+  
+  test "should return the winning team" do
+    assert_equal countries(:argentina).id, matches(:one).winning_country.id
+  end
 
 end

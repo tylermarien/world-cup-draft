7e5377ba8ff439c549028a7ee89902caf4ebb639
Tyler Marien <tyler.marien@gmail.com>
2014-06-09 22:07:26 -0600

    Added points from matches played

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,15 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :players
   
   def calculate_total
+    self.calculate_points_from_goals + self.calculate_points_from_matches_played
+  end
+  
+  def calculate_points_from_goals
     players.inject(0) { |sum, p| sum + (p.calculate_goals * 3) }
+  end
+  
+  def calculate_points_from_matches_played
+    countries.inject(0) { |sum, c| sum + c.matches_played }
   end
   
   def to_s
--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -4,7 +4,17 @@ class TeamTest < ActiveSupport::TestCase
   
   test "should return the correct total" do
     tyler = teams(:tyler)
-    assert_equal 3, tyler.calculate_total
+    assert_equal 4, tyler.calculate_total
+  end
+  
+  test "should return the correct points from goals" do
+    tyler = teams(:tyler)
+    assert_equal 3, tyler.calculate_points_from_goals    
+  end
+  
+  test "should return the correct points from matches played" do
+    tyler = teams(:tyler)
+    assert_equal 1, tyler.calculate_points_from_matches_played
   end
   
 end

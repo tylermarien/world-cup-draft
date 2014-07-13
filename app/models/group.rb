e8dc6343689c35b03f85bd69ebee5e5f83c01e13
Tyler Marien <tyler.marien@gmail.com>
2014-07-13 12:06:11 -0600

    Fixed group field on country form

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,15 @@
 class Group < ActiveRecord::Base
   has_many :countries
-  
+
   def completed?
     countries.each do |c|
       return false if c.matches_played < 3
     end
     return true
   end
-  
+
+  def to_s
+    name
+  end
+
 end
